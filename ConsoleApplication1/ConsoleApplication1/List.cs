4e48ca620703200cf38237841effb2a045e1c009
sondang96z <sondang96z@gmail.com>
2016-09-26 09:22:57 +0700

    commit

---
+++ ConsoleApplication1/ConsoleApplication1/List.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class List<T> 
+    {
+        private Element<T> head;
+
+        internal Element<T> Head
+        {
+            get { return head; }
+            set { head = value; }
+        }
+        public List()
+        { }
+        public void addfisrt(Element<T> s)
+        {
+            s.Next = head;
+            this.head = s;
+
+        }
+        
+    }
+}
